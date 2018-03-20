aa7154cda053f9d1ef3c2a61dd3444a9887c8288
Marco Giovinazzi <marco.giovinazzi@comodojo.org>
2018-03-21 00:32:07 +0100

    add initial commands

---
+++ src/Comodojo/Dispatcher/Commands/CacheClear.php
@@ -0,0 +1,42 @@
+<?php namespace Comodojo\Dispatcher\Commands;
+
+use \Comodojo\Foundation\Console\AbstractCommand;
+use \Symfony\Component\Console\Input\InputInterface;
+use \Symfony\Component\Console\Output\OutputInterface;
+use \Comodojo\SimpleCache\Manager as SimpleCacheManager;
+use \Comodojo\Foundation\Logging\Manager as LogManager;
+use \Comodojo\Dispatcher\Components\Version;
+
+class CacheClear extends AbstractCommand {
+
+    protected function configure() {
+
+        $this->setName('cache:clear')
+        ->setDescription('Clear dispatcher cache')
+        ->setHelp('This command will clear the whole dispatcher cache');
+
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+
+        $configuration = $this->getConfiguration();
+
+        $logger = LogManager::create('console', false)->getLogger();
+        $manager = SimpleCacheManager::createFromConfiguration($configuration, $logger);
+        $version = new Version($configuration);
+
+        $output->writeln([
+            '',
+            $version->getName()." (".$version->getVersion().")",
+            '-----------------------------------',
+            ''
+        ]);
+
+        $output->write('Clearing cache... ');
+        $manager->clear();
+        $output->write('done!');
+        $output->writeln('');
+
+    }
+
+}
